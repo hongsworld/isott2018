3c45184e222ab429a3fd25a3c3e649e2880b43d5
EC2 Default User <ec2-user@ip-172-31-15-54.ap-northeast-2.compute.internal>
2018-01-17 08:18:41 +0000

    prototype

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,78 @@
+class HomeController < ApplicationController
+	def index
+	end
+	
+	def conference_venue
+		render layout: 'post'
+	end
+
+	def highlight_seoul
+		render layout: 'post'
+	end
+
+
+	def scientific_program
+		render layout: 'post'
+	end
+
+	def guidelines
+		render layout: 'post'
+	end
+
+	def invited_speaker
+		render layout: 'post'
+	end
+
+	def social_program
+		render layout: 'post'
+	end
+
+	def organizing_committees
+		render layout: 'post'
+	end
+
+
+	def online_registration
+		render layout: 'post'
+	end
+
+	def abstract_preparation
+		render layout: 'post'
+	end
+
+
+	def accommodation
+		render layout: 'post'
+	end
+
+	def how_to_reach
+		render layout: 'post'
+	end
+
+
+	def award_general
+		render layout: 'post'
+	end
+
+	def award_melvin
+		render layout: 'post'
+	end
+
+	def award_dietrich
+		render layout: 'post'
+	end
+
+	def award_britton
+		render layout: 'post'
+	end
+
+	def award_duane
+		render layout: 'post'
+	end
+
+
+	def manuscript
+		render layout: 'post'
+	end
+
+end
